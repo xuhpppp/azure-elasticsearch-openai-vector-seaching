98ceac3ecc0729ec78316fdf69e2a458ba85c8b8
phucdt <doanthephuc.ST@gmail.com>
2024-09-21 17:26:27 +0700

    update cloud-init.sh

--- elasticsearch/cloud-init.sh
+++ elasticsearch/cloud-init.sh
@@ -1,19 +1,18 @@
-# setting ElasticSearch without security
-
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-sudo apt update
-sudo apt install elasticsearch -y
-sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
-sudo echo "xpack.ml.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's/xpack.security.enabled: true/xpack.security.enabled: false/' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i 's/xpack.security.enrollment.enabled: true/xpack.security.enrollment.enabled: false/' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i '/xpack.security.http.ssl:/,/keystore.path:/s/enabled: true/enabled: false/' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i '/xpack.security.transport.ssl:/,/keystore.path:/s/enabled: false/enabled: true/' /etc/elasticsearch/elasticsearch.yml
-sudo systemctl start elasticsearch
-sudo systemctl enable elasticsearch
+#!/bin/sh
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-8.x.list
+apt update
+apt install elasticsearch -y
+echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+echo "xpack.ml.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+sed -i 's/xpack.security.enabled: true/xpack.security.enabled: false/' /etc/elasticsearch/elasticsearch.yml
+sed -i 's/xpack.security.enrollment.enabled: true/xpack.security.enrollment.enabled: false/' /etc/elasticsearch/elasticsearch.yml
+sed -i '/xpack.security.http.ssl:/,/keystore.path:/s/enabled: true/enabled: false/' /etc/elasticsearch/elasticsearch.yml
+sed -i '/xpack.security.transport.ssl:/,/keystore.path:/s/enabled: false/enabled: true/' /etc/elasticsearch/elasticsearch.yml
+systemctl start elasticsearch
+systemctl enable elasticsearch
 # index medicine_vectors initiation
 curl --location --request PUT 'http://localhost:9200/medicine_vectors' \
 --header 'Content-Type: application/json' \
